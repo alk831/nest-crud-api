8aceb2c824ca53d4a295dd76f450ddd11cf3db82
alk831 <adam54111@gmail.com>
2019-07-15 14:09:13 +0200

    remove mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CardsModule } from './cards/cards.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL, { useNewUrlParser: true }),
     CardsModule
   ],
   controllers: [AppController],
